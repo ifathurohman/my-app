83b7164f6ac680fc4b6b4a4d479c1ec99f51de58
I Fathurohman <faturahman.ilham@gmail.com>
2023-05-10 16:22:27 +0700

    form & validation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import Komponen from './Tugas/ClassComponent';
 // import Props from './Tugas/Props';
-import Bootstrap from "./Tugas/bootstrap";
+// import Bootstrap from "./Tugas/bootstrap";
+import Form from "./Tugas/form";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Bootstrap />
+      <Form />
     </div>
   );
 }
